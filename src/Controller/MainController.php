1229f0c66f1872a8571a7e3c47332f3e445dbea0
MelonSal2 <72802705+MelonSal2@users.noreply.github.com>
2022-02-09 14:15:30 +0100

    Ajout navbar type restaurant

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use App\Repository\RestaurantRepository;
+use App\Repository\RestaurantTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,7 @@ class MainController extends AbstractController
     /**
      * @Route("/main", name="main")
      */
-    public function index(RestaurantRepository $restaurantRepository): Response
+    public function index(RestaurantRepository $restaurantRepository,RestaurantTypeRepository $restaurantTypeRepository): Response
     {
         $restaurantC = $restaurantRepository->findBy(['relation_type'=>3]);
         $restaurantFF= $restaurantRepository->findBy(['relation_type'=>1]);
@@ -24,17 +25,18 @@ class MainController extends AbstractController
             'restaurantC'=>$restaurantC,
             'restaurantFF'=>$restaurantFF,
             'restaurantP'=>$restaurantP,
-            
+            'restaurant_types' => $restaurantTypeRepository->findAll(),
         ]);
     }
        /**
      * @Route("/produitrestaurant/{id}", name="produitrestaurant")
      */
-    public function show(RestaurantRepository $restaurantRepository,ProductRepository $product,$id): Response
+    public function show(RestaurantRepository $restaurantRepository,ProductRepository $product,$id,RestaurantTypeRepository $restaurantTypeRepository): Response
     {
         return $this->render('main/listeproduit.html.twig', [
             'restaurants' => $restaurantRepository->findAll(),
             'products'=>$product->findBy(['relation_restaurant'=> $id]),
+            'restaurant_types' => $restaurantTypeRepository->findAll(),
         ]);
     }
            /**
--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Restaurant;
 use App\Form\RestaurantType;
 use App\Repository\RestaurantRepository;
+use App\Repository\RestaurantTypeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
